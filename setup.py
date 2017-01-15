dc0aea52579c0ca8997b98a252dff9052b4bb7be
Tom Barbette <tom.barbette@ulg.ac.be>
2017-01-15 17:22:54 +0000

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+desc = """\
+Network Performance Framework
+"""
+
+install_requires=['python>=3',
+          'numpy',
+          'regex',
+          'matplotlib',
+          'gitpython',
+          'typing'
+          ]
+
+
+setup(name='npf',
+      version='1.0',
+      packages=['src'],
+      author='Tom Barbette',
+      license='GPL',
+      author_email='tom.barbette@ulg.ac.be',
+      long_description=desc,
+      py_modules=['watcher','regression'],
+      url='http://github.com/tbarbette/clickwatcher/',
+      )
