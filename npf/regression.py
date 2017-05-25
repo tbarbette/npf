afbb32da9230b8dd45743a797a75d49d446d5d44
Tom Barbette <tom.barbette@ulg.ac.be>
2017-05-25 16:06:06 -0400

    Update regression for multi-type

--- npf/regression.py
+++ npf/regression.py
@@ -20,7 +20,7 @@ class Regression:
         accept += abs(result.std() * testie.config["accept_variance"] / n)
         return diff <= accept, diff
 
-    def compare(self, testie, variable_list, all_results, build, old_all_results, last_build,
+    def compare(self, testie, variable_list, all_results:Dataset, build, old_all_results, last_build,
                 allow_supplementary=True) -> tuple:
         """
         Compare two sets of results for the given list of variables and returns the amount of failing test
@@ -39,43 +39,46 @@ class Regression:
         for v in variable_list:
             tests_total += 1
             run = Run(v)
-            result = all_results.get(run)
+            results_types = all_results.get(run)
             # TODO : some config could implement acceptable range no matter the old value
-            if result is None:
+            if results_types is None or len(results_types) == 0:
                 continue
-            if old_all_results and run in old_all_results and not old_all_results[run] is None:
-                old_result = old_all_results[run]
-                ok, diff = self.accept_diff(testie, result, old_result)
-                if not ok and testie.config["n_supplementary_runs"] > 0 and allow_supplementary:
-                    if not testie.options.quiet:
+            for result_type,result in results_types.items():
+                if old_all_results and run in old_all_results and not old_all_results[run] is None:
+                    old_result = old_all_results[run].get(result_type,None)
+                    if old_result is None:
+                        continue
+                    ok, diff = self.accept_diff(testie, result, old_result)
+                    if not ok and testie.config["n_supplementary_runs"] > 0 and allow_supplementary:
+                        if not testie.options.quiet:
+                            print(
+                                "Difference of %.2f%% is outside acceptable margin for %s. Running supplementary tests..." % (
+                                    diff * 100, run.format_variables()))
+                        for i in range(testie.config["n_supplementary_runs"]):
+                            n, output, err = testie.execute(build, run, v)
+                            if not n:
+                                result = False
+                                break
+                            result += n
+
+                        if result:
+                            all_results[run] = result
+                            ok, diff = self.accept_diff(testie, result, old_result)
+                        else:
+                            ok = True
+
+                    if not ok:
                         print(
-                            "Difference of %.2f%% is outside acceptable margin for %s. Running supplementary tests..." % (
-                                diff * 100, run.format_variables()))
-                    for i in range(testie.config["n_supplementary_runs"]):
-                        n, output, err = testie.execute(build, run, v)
-                        if not n:
-                            result = False
-                            break
-                        result += n
-
-                    if result:
-                        all_results[run] = result
-                        ok, diff = self.accept_diff(testie, result, old_result)
+                            "ERROR: Test %s is outside acceptable margin between %s and %s : difference of %.2f%% !" % (
+                            testie.filename, build.version, last_build.version, diff * 100))
                     else:
-                        ok = True
-
-                if not ok:
-                    print(
-                        "ERROR: Test %s is outside acceptable margin between %s and %s : difference of %.2f%% !" % (
-                        testie.filename, build.version, last_build.version, diff * 100))
-                else:
-                    tests_passed += 1
-                    if not testie.options.quiet:
-                        print("Acceptable difference of %.2f%% for %s" % ((diff * 100), run.format_variables()))
-            elif last_build:
-                print("No old values for %s for version %s." % (run, last_build.version))
-                if (old_all_results):
-                    old_all_results[run] = [0]
+                        tests_passed += 1
+                        if not testie.options.quiet:
+                            print("Acceptable difference of %.2f%% for %s" % ((diff * 100), run.format_variables()))
+                elif last_build:
+                    print("No old values for %s for version %s." % (run, last_build.version))
+                    if (old_all_results):
+                        old_all_results[run] = [0]
         return tests_passed, tests_total
 
     def regress_all_testies(self, testies: List['Testie'], options, history : int = 1) -> Tuple[Build, List[Dataset]]:
