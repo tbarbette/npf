3c98f87c296cc1df81e55ad13a556b7c6f4a1e79
Tom Barbette <tom.barbette@ulg.ac.be>
2017-07-04 12:08:39 +0200

    Watcher test

--- integration/integration.sh
+++ integration/integration.sh
@@ -4,6 +4,7 @@
 compare() {
     test=$1
     python=$2
+    echo "Executing testie $test..."
     $python npf-run.py click-2017 --force-test --testie integration/$test.testie --quiet-build &> int_res
     if [ $? -ne 0 ] ; then
         echo "npf-run.py returned an error for test $test !"
@@ -18,7 +19,27 @@ compare() {
         echo "Command : $python npf-run.py click-2017 --force-test --testie integration/$test.testie --quiet-build"
         diff int_res integration/$test.stdout
     fi
+}
 
+#Function that launch watcher on a testie with click-2017 and compare the expected output
+compare_watcher() {
+    test=watcher
+    python=$1
+    echo "Executing watcher test..."
+    $python npf-watch.py click-2017 --force-test --testie tests/click/ --quiet-build --quiet --quiet-regression --onerun --history 2 --tags fast-regression &> int_res
+    if [ $? -ne 0 ] ; then
+        echo "npf-watch.py returned an error for test $test !"
+        cat int_res
+        exit 1
+    fi
+    cmp int_res integration/$test.stdout
+    if [ $? -eq 0 ] ; then
+        echo "$test passed !"
+    else
+        echo "Error for $test : expected output does not match !"
+        echo "Command : $python npf-run.py click-2017 --force-test --testie integration/$test.testie --quiet-build"
+        diff int_res integration/$test.stdout
+    fi
 }
 
 #Function to launch a test and just check the output
@@ -38,6 +59,7 @@ else
     python=python
 fi
 
-try tests/tcp/01-iperf.testie $python
-compare integration-01 $python
-compare integration-02 $python
+#try tests/tcp/01-iperf.testie $python
+#compare integration-01 $python
+#compare integration-02 $python
+compare_watcher $python
