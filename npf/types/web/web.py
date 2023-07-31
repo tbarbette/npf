33643847f30ac7cc38abe324c370bc8ab5df6152
dpcodebiz <43727605+dpcodebiz@users.noreply.github.com>
2023-07-31 20:20:56 +0200

    feat: passing axis name to npf-web-extension

--- npf/types/web/web.py
+++ npf/types/web/web.py
@@ -8,12 +8,18 @@ def prepare_web_export(datasets, all_results_df, path):
     name = "undefined"
     parameters = []
     measurements = []
+    x_axis = ""
+    y_axis = ""
     for test, _, runs in datasets:
       name = test.get_title()
       for run, results in runs.items():
         measurements = list(results.keys())
         parameters = list(run.variables.keys())
         break
+
+      for var, var_name in test.config["var_names"]:
+        x_axis = var_name if len(parameters) > 0 and var == parameters[0] else parameters[0]
+        y_axis = var_name if len(measurements) > 0 and var == measurements[0] else measurements[0]
       break
 
     # Preparing configuration data
@@ -22,7 +28,35 @@ def prepare_web_export(datasets, all_results_df, path):
         "name": name,
         "parameters": parameters,
         "measurements": measurements,
-        "data": all_results_df.to_csv(index=True, index_label="index", sep=",", header=True)
+        "data": all_results_df.to_csv(index=True, index_label="index", sep=",", header=True),
+        "settings": {
+          "x": {
+            "title": x_axis,
+            "parameter": parameters[0] if len(parameters) > 0 else "undefined",
+            "scale": 1
+          },
+          "y": {
+            "title": y_axis,
+            "parameter": measurements[0] if len(measurements) > 0 else "undefined",
+            "scale": 1
+          },
+          "split": {
+            "x": {
+                "enable": False,
+                "parameter": "",
+                "format": "",
+                "placement": "before"
+            },
+            "y": {
+                "enable": False,
+                "parameter": "",
+                "format": "",
+                "placement": "before"
+            },
+          },
+          "type": 0,
+          "error_bars": False
+        }
     }
 
     # Exporting
