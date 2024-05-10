f5317b1aff6d4716890ee0ce872491f38a0fb2c7
Tom Barbette <tom.barbette@uclouvain.be>
2024-05-10 07:45:43 +0200

    Add requirement

--- npf/test.py
+++ npf/test.py
@@ -405,16 +405,13 @@ class Test:
             if not s.noparse:
                 s.filename = SectionVariable.replace_variables(v, s.filename, role, default_role_map = self.config.get_dict("default_role_map"))
                 p = SectionVariable.replace_variables(v, s.content, role,default_role_map = self.config.get_dict("default_role_map"))
-                if s.jinja:
-                    from jinja2 import Environment, PackageLoader, select_autoescape
-                    env = Environment(
-                        loader=PackageLoader("npf"),
-                        autoescape=select_autoescape()
-                    )
-                    template = env.from_string(p)
-                    p = template.render(**v)
             else:
                 p = s.content
+            if s.jinja:
+                    from jinja2 import Environment, BaseLoader
+                    env = Environment(loader=BaseLoader)
+                    template = env.from_string(p)
+                    p = template.render(**v)
             create_list.append((s.filename, p, role))
         return create_list
 
