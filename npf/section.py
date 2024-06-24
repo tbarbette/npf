1e593049c3d32315f1aae6c5a8dc811221609160
Tom Barbette <tom.barbette@uclouvain.be>
2024-06-24 10:24:07 +0200

    Fix jinja on require section

--- npf/section.py
+++ npf/section.py
@@ -35,11 +35,11 @@ class SectionFactory:
     varPattern = "([a-zA-Z0-9_:-]+)[=](" + Variable.VALUE_REGEX + ")?"
     namePattern = re.compile(
         "^(?P<tags>" + Variable.TAGS_REGEX + "[:])?(?P<name>info|config|variables|exit|pypost|pyexit|late_variables|"
-        "include (?P<includeName>[a-zA-Z0-9_./-]+)(?P<includeParams>([ \t]+" + varPattern + ")+)?|"
-        "(init-)?file(:?[@](?P<fileRole>[a-zA-Z0-9]+))? (?P<fileName>[a-zA-Z0-9_.${}-]+)(:? (?P<fileNoparse>noparse))?(:? (?P<fileJinja>jinja))?|"
-        "require|"
+        "include\s+(?P<includeName>[a-zA-Z0-9_./-]+)(?P<includeParams>([ \t]+" + varPattern + ")+)?|"
+        "(init-)?file(:?[@](?P<fileRole>[a-zA-Z0-9]+))?\s+(?P<fileName>[a-zA-Z0-9_.${}-]+)(:? (?P<fileNoparse>noparse))?(:? (?P<fileJinja>jinja))?|"
+        "require(:?\s+(?P<requireJinja>jinja))?|"
         "import(:?[@](?P<importRole>[a-zA-Z0-9]+)(:?[-](?P<importMulti>[*0-9]+))?)?[ \t]+(?P<importModule>" + Variable.VALUE_REGEX + ")(?P<importParams>([ \t]+" + varPattern + ")+)?|"
-        "sendfile(:?[@](?P<sendfileRole>[a-zA-Z0-9]+))?[ \t]+(?P<sendfilePath>.*)|" +
+        "sendfile(:?[@](?P<sendfileRole>[a-zA-Z0-9]+))?\s+(?P<sendfilePath>.*)|" +
         "(:?script|init|exit)(:?[@](?P<scriptRole>[a-zA-Z0-9]+)(:?[-](?P<scriptMulti>[*0-9]+))?)?(:? (?P<scriptJinja>jinja))?(?P<scriptParams>([ \t]+" + varPattern + ")*))$")
 
     @staticmethod
@@ -112,7 +112,7 @@ class SectionFactory:
             s = SectionImport(None, matcher.group('includeName').strip(), params, is_include=True)
             return s
         elif sectionName == 'require':
-            s = SectionRequire()
+            s = SectionRequire(jinja=matcher.group('requireJinja'))
             return s
         elif sectionName == 'late_variables':
             s = SectionLateVariable()
@@ -282,9 +282,10 @@ class SectionInitFile(SectionFile):
 
 
 class SectionRequire(Section):
-    def __init__(self):
+    def __init__(self, jinja=False):
         super().__init__('require')
         self.content = ''
+        self.jinja = jinja
 
     def role(self):
         # For now, require is only on one node, the default one
