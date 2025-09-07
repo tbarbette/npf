673a9b3ca9512546dbde6c9ab12e87fb94f6371a
Tom Barbette <tom.barbette@uclouvain.be>
2025-09-07 18:35:20 +0100

    Add examples for variables

--- doc/build_graphs.sh
+++ doc/build_graphs.sh
@@ -8,3 +8,4 @@ python3 ./npf.py 'local:Add' --test doc/example_multiple_result.npf --graph-size
 python3 ./npf.py 'local:Append' --test doc/example_multiple_result.npf --graph-size 3 2.5 --graph-filename doc/example_multiple_result_append.png --config 'result_append={THROUGHPUT}' --force-retest
 python3 ./npf.py local:Namespace --test doc/example_namespaces.npf --graph-size 4 2.5 --graph-filename doc/example_namespaces.svg
 python3 ./npf.py 'local:Namespace (synced)' --test doc/example_namespaces.npf --graph-size 4 2.5 --graph-filename doc/example_namespaces_synced.svg --config 'var_sync={TIME}'
+python3 ./npf.py --test examples/doc-variable-example.npf --graph-size 3 2 --graph-filename examples/doc-variable-example.png
