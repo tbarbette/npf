e0ad42f28f4dc6cf945a17873989975aa5136ec5
Tom Barbette <tom.barbette@uclouvain.be>
2026-05-18 10:21:01 +0200

    Enoslib: Pass the extra_vars parameter

--- npf/executor/enoslibexecutor.py
+++ npf/executor/enoslibexecutor.py
@@ -13,39 +13,18 @@ import enoslib as en
 
 
 class EnoslibExecutor(Executor):
-    def __init__(self, machine):
+    def __init__(self, machine, extra_vars: dict = {}):
         super().__init__()
         self.machine = machine
-        #en.set_config(ansible_stdout="noop")
-        #p = en.run_command("echo 'test'", roles=machine)
-        #print(p)
+        self.extra_vars = extra_vars
 
-    def exec(self, cmd : str, bin_paths : List[str]=[],
-             queue: Queue = None, options = None,
-             stdin = None, timeout = None, sudo = False,
-             testdir = None, event = None, title = None, env = {}, virt = "", nokill=False
-        ) -> [int, str, str, int]:
-        """Runs a command in local
-
-        Args:
-            cmd (_type_): The command to run
-            bin_paths (List[str], optional): Paths to binaries, to be added to $PATH. Defaults to [].
-            queue (Queue, optional): Killing queue, to add this script to the list of script to be killed when the test is finished. Defaults to None.
-            options (_type_, optional): Global options. Defaults to None.
-            stdin (_type_, optional): stdin to echo to the script. Defaults to None.
-            timeout (_type_, optional): Timeout after which the execution is cancelled. Defaults to None.
-            sudo (bool, optional): If su priviledge should be gain. Defaults to False.
-            testdir (_type_, optional): Path in which to execute tests. Defaults to None.
-            event (_type_, optional): Event queue. Defaults to None.
-            title (_type_, optional): Title for the script. Defaults to None.
-            env (dict, optional): Env array. Defaults to {}.
-            virt (str, optional): Virtualisation decorator (eg namespaces). Defaults to "".
-
-        Returns:
-            [int, str, str, int]: pid, stdout, stderr, return code
-        """
+    def exec(self, cmd: str, bin_paths: List[str] = [],
+             queue: Queue = None, options=None,
+             stdin=None, timeout=None, sudo=False,
+             testdir=None, event=None, title=None, env={}, virt="", nokill=False
+             ) -> [int, str, str, int]:
         if testdir:
-            cmd = "mkdir -p " + testdir + " && cd " + testdir + ";\n" + cmd;
+            cmd = "mkdir -p " + testdir + " && cd " + testdir + ";\n" + cmd
 
         if not title:
             title = self.machine.address
@@ -62,7 +41,7 @@ class EnoslibExecutor(Executor):
             print("Executing (PATH+=%s) :\n%s" % (':'.join(bin_paths), cmd.strip()))
 
         if sudo and pwd.getpwuid(os.getuid()).pw_name != "root":
-            cmd = "sudo -E " + virt + "  bash -c '"+ cmd.replace("'", "'\"'\"'") + "'";
+            cmd = "sudo -E " + virt + "  bash -c '" + cmd.replace("'", "'\"'\"'") + "'"
         else:
             cmd = virt + " bash -c '"+ cmd.replace("'", "'\"'\"'") + "'";
 
@@ -71,24 +50,32 @@ class EnoslibExecutor(Executor):
 #            queue.put(killer)
 
         try:
-            p = en.run_command(cmd, roles=self.machine)
-        except en.errors.EnosFailedHostsError as e:
-            print("Error while running command!")
-            print(e)
-            return -1,"","",-1
+            p = en.run_command(cmd, roles=self.machine, extra_vars=self.extra_vars)
+        except (en.errors.EnosUnreachableHostsError, en.errors.EnosFailedHostsError) as e:
+            err = str(e)
+            print(f"Host {self.machine.address} is unreachable: {e}")
+            if "Could not resolve hostname" in err and "grid5000" in err:
+                print(
+                    "\nHint: Grid5000 hostnames are only reachable from inside G5K. "
+                    "If connecting from outside, add a ProxyCommand to ~/.ssh/config:\n\n"
+                    "    Host *.grid5000.fr\n"
+                    "        User <your-g5k-login>\n"
+                    "        ProxyCommand ssh <your-g5k-login>@access.grid5000.fr -W %h:%p\n"
+                    "        ForwardAgent no\n\n"
+                    "See: https://www.grid5000.fr/w/Getting_Started"
+                    "#Recommended_configuration_of_the_SSH_client"
+                )
+            return -1, "", "", -1
 
         p = p[0]
         for line in p.stdout.splitlines():
-
-            #line = line.decode()
             self.searchEvent(line, event)
             if options and not options.quiet:
                 self._print(title, line.rstrip(), True)
 
         return p.status, p.stdout, p.stderr, p.rc
 
-    def writeFile(self,filename,path_to_root,content,sudo=False):
+    def writeFile(self, filename, path_to_root, content, sudo=False):
         with en.actions(roles=self.machine) as a:
             a.copy(dest=filename, content=content)
-
         return True
